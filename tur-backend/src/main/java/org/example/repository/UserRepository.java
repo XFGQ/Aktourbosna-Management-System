f731478a2838422b126c344f95dbc2be6506f28a
Erennns1 <154510738+Erennns1@users.noreply.github.com>
2026-04-21 19:26:24 +0300

    feat: models files created

--- tur-backend/src/main/java/org/example/repository/UserRepository.java
+++ tur-backend/src/main/java/org/example/repository/UserRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
     // Spring bu ismi analiz eder ve otomatik sorgu oluşturur
     User findByUsername(String username);
-}
+} 
+ 
