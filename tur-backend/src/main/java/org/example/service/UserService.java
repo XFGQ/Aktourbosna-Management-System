b676e7a014f9e326e83f10ba4618a0fb19914daf
XFGQ <furkanduman1925@gmail.com>
2026-04-29 22:06:49 +0200

    refactor: vehicle id removed from dto,auto creating guide after creating user. post added to guide

--- tur-backend/src/main/java/org/example/service/UserService.java
+++ tur-backend/src/main/java/org/example/service/UserService.java
@@ -3,7 +3,9 @@ package org.example.service;
 import org.example.application.dto.UserRegistrationDTO;
 import org.example.application.dto.UserResponseDTO;
 import org.example.application.mapper.UserMapper;
+import org.example.model.Guide;
 import org.example.model.User;
+import org.example.repository.GuideRepository;
 import org.example.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +16,12 @@ import java.util.stream.Collectors;
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final GuideRepository guideRepository;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, GuideRepository guideRepository) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.guideRepository = guideRepository;
     }
 
     public List<UserResponseDTO> getAllUsers() {
@@ -29,6 +33,10 @@ public class UserService {
     public UserResponseDTO createUser(UserRegistrationDTO registrationDTO) {
         User user = userMapper.toEntity(registrationDTO);
         User savedUser = userRepository.save(user);
+            Guide newGuide = Guide.builder()
+                    .user(savedUser)
+                    .build();
+            guideRepository.save(newGuide);
         return userMapper.toResponseDto(savedUser);
     }
 }
